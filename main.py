08863920f52a4cb700a66106ffa53b9640606eea
Enasan <119594303+btrh@users.noreply.github.com>
2023-01-21 21:06:49 +0900

    remove modo

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import modo
+# import modo
 if __name__ == '__main__':
     # ab = mod.moda()
-    ab = modo.moda()
-    ab.fal()
+    # ab = modo.moda()
+    # ab.fal()
     # mod.moda.fal(self=mod.moda)
     # mod.moda.fal(self=mod.moda)
+    print("this is main.py no end")
