d01e097279668ea83f1f090c8ff9115b1d989902
Enasan <119594303+btrh@users.noreply.github.com>
2023-01-21 21:17:27 +0900

    Create test3 copy.py

---
+++ test3 copy.py
@@ -0,0 +1,12 @@
+import mod
+if __name__ == '__main__':
+    # ab = mod.moda()
+    ab = mod.moda()
+    ab.fal()
+    ab.fal()
+    ab.fal()
+    ab.fal()
+    ab.fal()
+    ab.fal()
+    # mod.moda.fal(self=mod.moda)
+    # mod.moda.fal(self=mod.moda)
