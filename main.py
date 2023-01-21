82f62ee50cff9824c9869285cd12f8b39237efc9
Enasan <119594303+btrh@users.noreply.github.com>
2023-01-21 21:07:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ if __name__ == '__main__':
     # ab = mod.moda()
     ab = mod.moda()
     ab.fal()
+    ab.fal()
+    ab.fal()
+    ab.fal()
     # mod.moda.fal(self=mod.moda)
     # mod.moda.fal(self=mod.moda)
